95840adbf1d40caea061798d078802e8845c6ddc
vagrant <vagrant@homestead>
2018-08-10 02:58:01 +0000

    criado migracao para tabela clientes

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -36,8 +36,9 @@ class ClienteController extends Controller
      */
     public function store(Request $request)
     {
-        dd($request->all());
-        // Cliente::create($request->except(''));
+        #dd($request->all());
+        Cliente::create($request->except('_token'));
+        return redirect('cliente');
     }
 
     /**
